f739f21c822616104fd3b044845de2cce8f12db1
d-hemms <hemmsde@mcmaster.ca>
2016-04-03 19:38:09 -0400

    commented my methods with TODO's where applicable

--- src/searchHistory.js
+++ src/searchHistory.js
@@ -1,15 +1,15 @@
 public class searchHistory{
 	var history = [];
-	function add(var car){
-		if (history.length <5){
+	function add(car){
+		if (history.length <5){	//if array not full, prepends new car to array
 			history.unshift(car);
 		}
-		else{
+		else{	//if history already full, removes 5th element and prepends new car to array
 			history.pop();
 			history.unshift(car);
 		}
 	}
-	function displayHistory(){
+	function displayHistory(){	//quick display method, may or may not be needed
 		var text;
 		for(i = 0; i < history.length; i++){
 			text += (i+1) + ". " + history[i] + "<br>";
