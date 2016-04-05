123716b36e7d97e8161e58f9f805d19c83c1b208
MightyRevenge <reach.yashg@gmail.com>
2016-04-05 05:55:08 -0400

    Experimenting with controllers and front-end

--- carspot-app/www/js/app.js
+++ carspot-app/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('home', ['ionic','lumx'])
+var homePage = angular.module('home', ['ionic','lumx'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,10 @@ angular.module('home', ['ionic','lumx'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+homePage.controller('homeCtrl',function($scope){
+    $scope.displayMessage = function(){
+        $scope.message = 'Welcome to CarSpot!';
+    }
+});
