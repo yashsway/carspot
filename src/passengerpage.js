f6234c9d62750f98bae0fda8bd649277cae045cc
Cole Huot <huotjc@mcmaster.ca>
2016-04-05 18:15:49 -0400

    updated index.html

---
+++ src/passengerpage.js
@@ -0,0 +1,25 @@
+//PASSENGER PAGE (EXPERT 4)
+var PassengerPage = new tabris.Page({
+  title: "Passengers ID",
+  topLevel: true
+});
+
+new tabris.TextView({
+  layoutData: {centerX: 0, centerY: 0},
+  text: "This is the passenger page\n wow",
+  alignment: "center"
+}).appendTo(PassengerPage);
+
+new tabris.Button({
+  layoutData: {centerX: 0, centerY: 100, width: 150},
+  text: "Next"
+}).on("select", function() {
+  VerifyPage.open();
+}).appendTo(PassengerPage);
+
+new tabris.Button({
+  layoutData: {centerX: 0, centerY: 150, width: 150},
+  text: "Back"
+}).on("select", function() {
+  OriginPage.open();
+}).appendTo(PassengerPage);
