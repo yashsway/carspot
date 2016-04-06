da5bd5c420e0d9ffdaaf7960d92044ae4fd497cc
MightyRevenge <reach.yashg@gmail.com>
2016-04-05 21:21:15 -0400

    Fixed view bug by removing ion-content tag wrapping around everything

--- carspot-app/www/js/app.js
+++ carspot-app/www/js/app.js
@@ -3,30 +3,53 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var homePage = angular.module('home', ['ionic','ui.bootstrap','lumx'])
+var homePage = angular.module('home', ['ionic', 'ui.bootstrap', 'lumx'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 });
-homePage.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-  $ionicConfigProvider.tabs.position('top');
+homePage.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+    $stateProvider
+        .state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+        })
+        .state('tabs.home', {
+            url: "/home",
+            views: {
+                'home-tab': {
+                    templateUrl: "templates/home.html"
+                }
+            }
+        })
+        .state('tabs.search', {
+            url: "/search",
+            views: {
+                'search-tab': {
+                    templateUrl: "templates/search.html"
+                }
+            }
+        });
+    $urlRouterProvider.otherwise("/tab/home");
 });
-homePage.controller('homeCtrl',function($scope){
-    $scope.displayMessage = function(){
+homePage.controller('homeCtrl', function ($scope) {
+    $scope.displayMessage = function () {
         $scope.message = 'Welcome to CarSpot!';
     }
 });
